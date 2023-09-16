6a15350280e3fbc9565a5a83bc1c07053c8e973f
fogelo <fogelo@yandex.ru>
2023-09-16 14:50:46 +0300

    обновил webpack cli

--- using-advanced-guide-editor/main.js
+++ using-advanced-guide-editor/main.js
@@ -1,14 +1,14 @@
-// main.js
+// // main.js
 
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
+import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Link } from "@ckeditor/ckeditor5-link";
+import { List } from "@ckeditor/ckeditor5-list";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
 
 ClassicEditor
     .create( document.querySelector( '#app'), {
@@ -44,3 +44,51 @@ ClassicEditor
     .catch( error => {
         console.error( error );
     } );
+
+// class Editor extends ClassicEditor {
+//   // Plugins to include in the build.
+//   static builtinPlugins = [
+//     Essentials,
+//     Autoformat,
+//     Bold,
+//     Italic,
+//     BlockQuote,
+//     Heading,
+//     Link,
+//     List,
+//     Paragraph,
+//   ];
+
+//   static defaultConfig = {
+//     toolbar: {
+//       items: [
+//         "heading",
+//         "bold",
+//         "italic",
+//         "link",
+//         "bulletedList",
+//         "numberedList",
+//         "blockQuote",
+//         "undo",
+//         "redo",
+//       ],
+//     },
+
+//     // This value must be kept in sync with the language defined in webpack.config.js.
+//     //   language: "en",
+//     //   image: {
+//     //     toolbar: [
+//     //       "imageTextAlternative",
+//     //       "toggleImageCaption",
+//     //       "imageStyle:inline",
+//     //       "imageStyle:block",
+//     //       "imageStyle:side",
+//     //     ],
+//     //   },
+//     //   table: {
+//     //     contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+//     //   },
+//   };
+// }
+
+// export default Editor;

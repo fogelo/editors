9084cbe469d019304dbf534496cfa77511416a10
fogelo <fogelo@yandex.ru>
2023-09-18 10:28:31 +0300

    подключен ckeditor-rect

--- using-advanced-guide-editor/main.tsx
+++ using-advanced-guide-editor/main.tsx
@@ -70,6 +70,8 @@ import { Base64UploadAdapter } from "@ckeditor/ckeditor5-upload";
 
 import ReactDOM from "react-dom/client";
 import React, { useEffect, useRef } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
 const App = () => {
   const ref = useRef(null);
 
@@ -154,7 +156,97 @@ const App = () => {
       });
   }, []);
 
-  return <div ref={ref}>hello</div>;
+  return (
+    <div>
+      <div ref={ref}>hello</div>
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          plugins: [
+            Essentials,
+            Autoformat,
+            Bold,
+            Italic,
+            BlockQuote,
+            Heading,
+            Link,
+            List,
+            Paragraph,
+            AutoImage,
+            Image,
+            ImageCaption,
+            ImageInsert,
+            ImageResize,
+            ImageStyle,
+            ImageToolbar,
+            ImageUpload,
+            Base64UploadAdapter,
+          ],
+          toolbar: [
+            "heading",
+            "bold",
+            "italic",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "blockQuote",
+            "undo",
+            "redo",
+            "insertImage",
+          ],
+          image: {
+            // styles: ["alignCenter", "alignLeft", "alignRight"],
+            resizeOptions: [
+              {
+                name: "resizeImage:original",
+                label: "Default image width",
+                value: null,
+              },
+              {
+                name: "resizeImage:50",
+                label: "50% page width",
+                value: "50",
+              },
+              {
+                name: "resizeImage:75",
+                label: "75% page width",
+                value: "75",
+              },
+            ],
+            toolbar: [
+              "imageTextAlternative",
+              "toggleImageCaption",
+              "|",
+              "imageStyle:inline",
+              "imageStyle:wrapText",
+              "imageStyle:breakText",
+              "imageStyle:side",
+              "|",
+              "resizeImage",
+            ],
+            insert: {
+              integrations: ["insertImageViaUrl"],
+            },
+          },
+        }}
+        // data="<p>Hello from CKEditor&nbsp;5!</p>"
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log("Focus.", editor);
+        }}
+      />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
